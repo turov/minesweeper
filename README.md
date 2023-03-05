5eed8d9c2ccc4795bbac89e580142015d1b19a6e
turov <ilya.turov@gmail.com>
2023-03-05 18:05:12 +0300

    📡

--- README.md
+++ README.md
@@ -1,46 +1,19 @@
-# Getting Started with Create React App
+# «Сапёр» (англ. Minesweeper) игра-головоломка 🤯
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+### ⚙️ Установка зависимостей:
+```
+npm install
+```
 
-## Available Scripts
+### 🚀 Запуск проекта с локальным сервером:
 
-In the project directory, you can run:
+```
+npm run start
+```
 
-### `npm start`
+### 🛠️ Cборка проекта в папку build :
+```
+npm run build
+```
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+#### Работающий пример в общем доступе [туть](https://turov.github.io/minesweeper/).
--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 import styles from "./Game.module.scss";
 
-const fieldSize: number = 16;
+const fieldSize: number = 4;
 const mineSymbol: number = -1;
-const mineAmount: number = 40;
+const mineAmount: number = 2;
 const availableTime: number = 999;
 const dimension = new Array(fieldSize).fill(null);
+const mineIndices: number[] = [];
 const getCellIndex = function (x: number, y: number): number {
   return y * fieldSize + x;
 };
 
-const mineIndices: number[] = [];
+const padNum = function (num: number): string {
+  if (num >= 1) {
+    return num.toString().padStart(3, "0");
+  }
+  return "000";
+};
+const inc = function (x: number, y: number, field: number[]) {
+  if (x >= 0 && x < fieldSize && y >= 0 && y < fieldSize) {
+    if (field[getCellIndex(x, y)] !== mineSymbol) {
+      return (field[getCellIndex(x, y)] += 1);
+    }
+  }
+};
 const createField = function (
   fieldSize: number,
   firstTurnCell: number
 ): number[] {
   mineIndices.length = 0;
   const field: number[] = new Array(Math.pow(fieldSize, 2)).fill(0);
-
-  const inc = function (x: number, y: number) {
-    if (x >= 0 && x < fieldSize && y >= 0 && y < fieldSize) {
-      if (field[getCellIndex(x, y)] === mineSymbol) return;
-      field[getCellIndex(x, y)] += 1;
-    }
-  };
 
   for (let i: number = 0; i < mineAmount; ) {
     const x = Math.floor(Math.random() * fieldSize);
@@ -38,38 +44,28 @@ const createField = function (
     mineIndices.push(getCellIndex(x, y));
     i += 1;
 
-    inc(x + 1, y);
-    inc(x - 1, y);
-    inc(x, y + 1);
-    inc(x, y - 1);
-    inc(x + 1, y - 1);
-    inc(x - 1, y - 1);
-    inc(x + 1, y + 1);
-    inc(x - 1, y + 1);
+    inc(x + 1, y, field);
+    inc(x - 1, y, field);
+    inc(x, y + 1, field);
+    inc(x, y - 1, field);
+    inc(x + 1, y - 1, field);
+    inc(x - 1, y - 1, field);
+    inc(x + 1, y + 1, field);
+    inc(x - 1, y + 1, field);
   }
 
   return field;
 };
-
-const padNum = function (num: number): string {
-  if (num >= 1) {
-    return num.toString().padStart(3, "0");
+const open = function (
+  x: number,
+  y: number,
+  newMask: Mask[],
+  clearing: [number, number][]
+) {
+  if (x >= 0 && x < fieldSize && y >= 0 && y < fieldSize) {
+    if (newMask[getCellIndex(x, y)] === Mask.Opened) return;
+    clearing.push([x, y]);
   }
-  return "000";
-};
-
-enum Mask {
-  Transparent,
-  Filled,
-  Flag,
-  Question,
-}
-
-const maskToView: Record<Mask, React.ReactNode> = {
-  [Mask.Transparent]: null,
-  [Mask.Filled]: "",
-  [Mask.Flag]: "🏳️",
-  [Mask.Question]: "❔",
 };
 
 const getFalseMines = function (a: number[], b: number[]): number[] {
@@ -80,54 +76,146 @@ const getFalseMines = function (a: number[], b: number[]): number[] {
   return result;
 };
 
+enum Mask {
+  Opened,
+  Closed,
+  Flag,
+  Question,
+}
+
+const maskToData: Record<Mask, React.ReactNode> = {
+  [Mask.Opened]: null,
+  [Mask.Closed]: "",
+  [Mask.Flag]: "🏳️",
+  [Mask.Question]: "❔",
+};
+
 const Game = () => {
   const [field, setField] = React.useState<number[]>();
   const [mask, setMask] = React.useState<Mask[]>(() => {
-    return new Array(fieldSize * fieldSize).fill(Mask.Filled);
+    return new Array(Math.pow(fieldSize, 2)).fill(Mask.Closed);
   });
   const [smile, setSmile] = React.useState<string>("🙂");
-  const [firstTurn, setFirstTurn] = React.useState<boolean>(true);
-  const [gameOver, setGameOver] = React.useState<boolean>(false);
-  const [win, setWin] = React.useState<boolean>(false);
   const [deadlyMine, setDeadlyMine] = React.useState<number>();
   const [seconds, setSeconds] = React.useState<number>();
   const [mineCounter, setMineCounter] = React.useState<number>(mineAmount);
   const [falseMines, setFalseMines] = React.useState<number[]>();
 
-  const winChecker = React.useMemo(() => {
+  const firstTurn = React.useRef<boolean>(true);
+  const gameOver = React.useRef<boolean>(false);
+  const win = React.useRef<boolean>(false);
+
+  React.useEffect(() => {
     if (field) {
-      let arr: boolean[] = [];
-      mask.filter((m, i) => {
-        if (mask[i] === 1 || mask[i] === 2) {
+      const arr: boolean[] = [];
+      mask.forEach((m, i) => {
+        if (mask[i] > 0) {
           arr.push(!!mineIndices.find((elem) => elem === i));
         }
       });
-      if (arr.every((item: boolean) => item === true)) {
-        setWin(true);
+      console.log(arr);
+      if (arr.every((item: boolean) => item)) {
+        win.current = true;
         setSmile("😎");
       }
     }
   }, [field, mask]);
+
   React.useEffect(() => {
-    if (seconds && seconds < availableTime && !win && !gameOver) {
+    if (
+      seconds &&
+      seconds < availableTime &&
+      !win.current &&
+      !gameOver.current
+    ) {
       const interval = setInterval(() => setSeconds(seconds + 1), 1000);
       return () => clearInterval(interval);
     }
   }, [seconds]);
 
   const resetGame = function () {
-    setFirstTurn(true);
-    setGameOver(false);
-    setWin(false);
+    firstTurn.current = true;
+    gameOver.current = false;
+    win.current = false;
     setDeadlyMine(undefined);
     setSmile("🙂");
     setMask(() => {
-      return new Array(fieldSize * fieldSize).fill(Mask.Filled);
+      return new Array(fieldSize * fieldSize).fill(Mask.Closed);
     });
     setField(undefined);
     setSeconds(0);
     setMineCounter(mineAmount);
     setFalseMines([]);
+  };
+
+  const onCellClick = function (x: number, y: number) {
+    if (mask[getCellIndex(x, y)] === Mask.Opened) return;
+    let currentField = field;
+    if (firstTurn.current) {
+      firstTurn.current = false;
+      const firstTurnCell = getCellIndex(x, y);
+      currentField = createField(fieldSize, firstTurnCell);
+      setField(currentField);
+      setSeconds(() => 1);
+    }
+    if (currentField) {
+      const newMask = [...mask];
+      const clearing: [number, number][] = [];
+
+      open(x, y, newMask, clearing);
+
+      while (clearing.length) {
+        const [x, y] = clearing.pop()!;
+        newMask[getCellIndex(x, y)] = Mask.Opened;
+
+        if (currentField[getCellIndex(x, y)] !== 0) continue;
+        open(x + 1, y, newMask, clearing);
+        open(x - 1, y, newMask, clearing);
+        open(x, y + 1, newMask, clearing);
+        open(x, y - 1, newMask, clearing);
+      }
+
+      if (currentField[getCellIndex(x, y)] === mineSymbol) {
+        gameOver.current = true;
+        setSmile("☠️");
+        setDeadlyMine(getCellIndex(x, y));
+        const flags: number[] = [];
+        newMask.forEach(function (x, index) {
+          if (x === 2) {
+            flags.push(index);
+          }
+        });
+        setFalseMines(getFalseMines(flags, mineIndices));
+        currentField.forEach((cell, idx) => {
+          if (cell === -1) {
+            newMask[idx] = Mask.Opened;
+          }
+        });
+      }
+      setMask(newMask);
+    }
+  };
+
+  const onContextMenuClick = function (
+    e: React.MouseEvent<HTMLElement>,
+    x: number,
+    y: number
+  ) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (win.current || gameOver.current || firstTurn.current) return;
+    setSmile("🙂");
+    const newMask = [...mask];
+    if (newMask[getCellIndex(x, y)] === Mask.Opened) return;
+    if (newMask[getCellIndex(x, y)] === Mask.Closed && mineCounter > 0) {
+      newMask[getCellIndex(x, y)] = Mask.Flag;
+    } else if (newMask[getCellIndex(x, y)] === Mask.Flag) {
+      newMask[getCellIndex(x, y)] = Mask.Question;
+    } else if (newMask[getCellIndex(x, y)] === Mask.Question) {
+      newMask[getCellIndex(x, y)] = Mask.Closed;
+    }
+    setMineCounter(mineAmount - newMask.filter((code) => code === 2).length);
+    setMask(newMask);
   };
 
   return (
@@ -151,84 +239,20 @@ const Game = () => {
                   <button
                     key={x}
                     className={`${styles["📦"]} `}
-                    disabled={gameOver || win}
+                    disabled={gameOver.current || win.current}
                     onMouseDown={() => {
-                      if (mask[getCellIndex(x, y)] === Mask.Transparent) return;
+                      if (mask[getCellIndex(x, y)] === Mask.Opened) return;
                       setSmile("😮");
                     }}
                     onMouseUp={() => {
-                      if (mask[getCellIndex(x, y)] === Mask.Transparent) return;
+                      if (mask[getCellIndex(x, y)] === Mask.Opened) return;
                       setSmile("🙂");
                     }}
-                    onClick={(event) => {
-                      if (mask[getCellIndex(x, y)] === Mask.Transparent) return;
-                      let currentField = field;
-                      if (firstTurn) {
-                        setFirstTurn(false);
-                        const firstTurnCell = getCellIndex(x, y);
-                        currentField = createField(fieldSize, firstTurnCell);
-                        setField(currentField);
-                        setSeconds(() => 1);
-                      }
-                      if (currentField) {
-                        const newMask = [...mask];
-                        const clearing: [number, number][] = [];
-
-                        // @ts-ignore
-                        function open(x: number, y: number) {
-                          if (
-                            x >= 0 &&
-                            x < fieldSize &&
-                            y >= 0 &&
-                            y < fieldSize
-                          ) {
-                            if (
-                              newMask[getCellIndex(x, y)] === Mask.Transparent
-                            )
-                              return;
-                            clearing.push([x, y]);
-                          }
-                        }
-
-                        open(x, y);
-
-                        while (clearing.length) {
-                          const [x, y] = clearing.pop()!;
-                          console.log(x);
-                          console.log(y);
-                          newMask[getCellIndex(x, y)] = Mask.Transparent;
-
-                          if (currentField[getCellIndex(x, y)] !== 0) continue;
-                          open(x + 1, y);
-                          open(x - 1, y);
-                          open(x, y + 1);
-                          open(x, y - 1);
-                        }
-
-                        if (currentField[getCellIndex(x, y)] === mineSymbol) {
-                          setGameOver(true);
-                          setSmile("☠️");
-                          setDeadlyMine(getCellIndex(x, y));
-                          const flags: number[] = [];
-                          newMask.forEach(function (x, index) {
-                            if (x === 2) {
-                              flags.push(index);
-                            }
-                          });
-                          setFalseMines(getFalseMines(flags, mineIndices));
-                          currentField.forEach((cell, idx) => {
-                            if (cell === -1) {
-                              newMask[idx] = Mask.Transparent;
-                            }
-                          });
-                        }
-                        setMask(newMask);
-                      }
-                    }}
+                    onClick={() => onCellClick(x, y)}
                     data-value={`${(() => {
                       if (!field) return;
-                      if (mask[getCellIndex(x, y)] !== Mask.Transparent) {
-                        if (falseMines && gameOver) {
+                      if (mask[getCellIndex(x, y)] !== Mask.Opened) {
+                        if (falseMines && gameOver.current) {
                           if (
                             falseMines.find(
                               (cell) => cell === getCellIndex(x, y)
@@ -236,7 +260,7 @@ const Game = () => {
                           ) {
                             return "🤥";
                           }
-                        } else return maskToView[mask[getCellIndex(x, y)]];
+                        } else return maskToData[mask[getCellIndex(x, y)]];
                       } else {
                         if (deadlyMine === getCellIndex(x, y)) {
                           return "🟥";
@@ -247,31 +271,7 @@ const Game = () => {
                         }
                       }
                     })()}`}
-                    onContextMenu={(event) => {
-                      event.preventDefault();
-                      event.stopPropagation();
-                      if (win || gameOver || firstTurn) return;
-                      setSmile("🙂");
-                      const newMask = [...mask];
-                      if (newMask[getCellIndex(x, y)] === Mask.Transparent)
-                        return;
-                      if (
-                        newMask[getCellIndex(x, y)] === Mask.Filled &&
-                        mineCounter > 0
-                      ) {
-                        newMask[getCellIndex(x, y)] = Mask.Flag;
-                      } else if (newMask[getCellIndex(x, y)] === Mask.Flag) {
-                        newMask[getCellIndex(x, y)] = Mask.Question;
-                      } else if (
-                        newMask[getCellIndex(x, y)] === Mask.Question
-                      ) {
-                        newMask[getCellIndex(x, y)] = Mask.Filled;
-                      }
-                      setMineCounter(
-                        mineAmount - newMask.filter((code) => code === 2).length
-                      );
-                      setMask(newMask);
-                    }}
+                    onContextMenu={(event) => onContextMenuClick(event, x, y)}
                   ></button>
                 );
               })}
